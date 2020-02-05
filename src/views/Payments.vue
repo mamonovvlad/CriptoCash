834738841271b71e097c03634d24d978d6dd022f
RemoteHelpers <Vlad@Mamonov.com>
2020-02-05 22:24:15 +0300

    finish

--- src/views/Payments.vue
+++ src/views/Payments.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>12</div>
+  <div class="df align-items-center justify-content-center flex-direction-column">
+    <h2>Выплаты</h2>
+    <form action class="df flex-direction-column align-items-center form-ent">
+      <input id="name" type="text" placeholder="Имя" />
+      <input id="email-log" type="text" placeholder="Email" />
+      <input type="number" placeholder="Номер счета" />
+      <button>Отправить</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'payments',
   metaInfo: {
     title: 'Выплаты'
-  }
+  },
+  data: () => ({
+  })
 }
 </script>
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -6,49 +6,49 @@
     </div>
     <div class="raffle-box df align-items-center justify-content-center">
       <div class="arrow-top">
-        <img src="src/img/raffle/arrow-top.svg" alt />
+        <img src="img/raffle/arrow-top.svg" alt />
       </div>
       <div class="arrow-bottom">
-        <img src="src/img/raffle/arrow-bottom.svg" alt />
+        <img src="img/raffle/arrow-bottom.svg" alt />
       </div>
       <div class="shadow-left">
-        <img src="src/img/raffle/shadow.svg" alt />
+        <img src="img/raffle/shadow.svg" alt />
       </div>
       <div class="shadow-right">
-        <img src="src/img/raffle/shadowtwo.svg" alt />
+        <img src="img/raffle/shadowtwo.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/one.svg" alt />
+        <img src="img/raffle/one.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/eight.svg" alt />
+        <img src="img/raffle/eight.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/two.svg" alt />
+        <img src="img/raffle/two.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/three.svg" alt />
+        <img src="img/raffle/three.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/four.svg" alt />
+        <img src="img/raffle/four.svg" alt />
       </div>
       <div class="raffle-box-block raffle-action">
-        <img src="src/img/raffle/five.svg" alt />
+        <img src="img/raffle/five.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/six.svg" alt />
+        <img src="img/raffle/six.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/seven.svg" alt />
+        <img src="img/raffle/seven.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/eight.svg" alt />
+        <img src="img/raffle/eight.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/nine.svg" alt />
+        <img src="img/raffle/nine.svg" alt />
       </div>
       <div class="raffle-box-block">
-        <img src="src/img/raffle/ten.svg" alt />
+        <img src="img/raffle/ten.svg" alt />
       </div>
     </div>
 
